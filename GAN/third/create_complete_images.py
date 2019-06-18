c873476026958150c8cded90d958ee8b8d9cc9d2
tillsch <tillsch@student.ethz.ch>
2019-06-18 14:30:35 +0200

    Added scoring function to image creator

--- GAN/third/create_complete_images.py
+++ GAN/third/create_complete_images.py
@@ -9,9 +9,11 @@ import yaml
 from Models import TanhLayer, SigmoidLayer
 from random import gauss, randint
 import matplotlib.pyplot as plt
+import joblib
 
 image_channels = 1
 image_size = 1000
+home_dir = os.path.expanduser("~")
 
 def overlap(x, y, stars_pos_list, dist_stars):
     for (xi, yi) in stars_pos_list:
@@ -27,7 +29,7 @@ def round_pos_int(some_decimal):
     return int(some_decimal) + 1
 
 
-def create_complete_images(gen_model, vmin=0, num_images_to_create=2):
+def create_complete_images(gen_model, vmin=0, num_images_to_create=100):
     num_stars_per_pic = []
     for i in range(num_images_to_create):
         num_stars_per_pic.append(round_pos_int(gauss(12.32, 4.04103)))
@@ -71,7 +73,17 @@ if __name__ == '__main__':
     gen_model = tf.keras.models.model_from_json(json_config, custom_objects=custom_objects)
     gen_model.load_weights(args.checkpoint_path[:-len('.data-00000-of-00001')])
     image_tensor = create_complete_images(gen_model, vmin=conf['vmin'])
-    for num_img in range(image_tensor.shape[0]):
-        img_np2 = image_tensor[num_img, :, :, 0]
-        plt.imshow(img_np2, cmap='gray', vmin=conf['vmin'], vmax=conf['vmax'])
-        plt.show()
+    hist_list = []
+    for i in range(image_tensor.shape[0]):
+        hist_list.append(np.histogram(image_tensor[i], bins=10)[0])
+    hist_tensor = np.stack(hist_list)
+    val_model = joblib.load(os.path.join(os.path.join(home_dir, "CIL_project/RandomForest"), "random_forest_10_0.9.sav"))
+    score = val_model.predict(hist_tensor)
+    label_eight = np.ones((image_tensor.shape[0], 1)) * 8
+    gen_val_loss = tf.reduce_mean(tf.square(score - label_eight))
+    print("MSE loss of generated images: {}".format(gen_val_loss))
+    if False:
+        for num_img in range(image_tensor.shape[0]):
+            img_np2 = image_tensor[num_img, :, :, 0]
+            plt.imshow(img_np2, cmap='gray', vmin=conf['vmin'], vmax=conf['vmax'])
+            plt.show()
