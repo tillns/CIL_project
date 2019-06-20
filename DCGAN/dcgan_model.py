37b1ea6dcde29ffbe23a95a2a3d78cfb05f6d5b1
Hannes <hannepfa@student.ethz.ch>
2019-06-20 23:33:46 +0200

    extended the DCGAN Python scripts

--- DCGAN/dcgan_model.py
+++ DCGAN/dcgan_model.py
@@ -1,11 +1,38 @@
+"""DCGAN Model
 
+This file contains two functions which create the generator model and the discriminator model. The code is based on the 
+DCGAN tutorial on the TensorFlow website (see https://www.tensorflow.org/alpha/tutorials/generative/dcgan). 
+
+The values for most hyperparameters are from the original DCGAN paper and code: 
+https://arxiv.org/abs/1511.06434
+https://github.com/Newmu/dcgan_code/blob/master/imagenet/load_pretrained.py (original code)
+https://github.com/carpedm20/DCGAN-tensorflow/blob/master/ops.py (referenced public TensorFlow implementation)
+
+"""
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# !pip install -q tensorflow-gpu==2.0.0-alpha0
 import tensorflow as tf
+
 from tensorflow.keras import layers
 
 
 def make_generator_model():
     
+    """Creates a convolutional neural network which serves as generator model
+    
+    
+    Returns
+    -------
+    model : tf.keras.Sequential
+        The generator model
+        
+    """
+    
     model = tf.keras.Sequential()
+    
     
     model.add(layers.Dense(63 * 63 * 64, use_bias=True, 
         kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.0002, seed=None),
@@ -15,7 +42,8 @@ def make_generator_model():
     model.add(layers.ReLU())
     model.add(layers.Reshape((63, 63, 64)))
     
-    assert model.output_shape == (None, 63, 63, 64) # note: None is the batch size
+    assert model.output_shape == (None, 63, 63, 64) # NOTE: None is the batch size
+    
 
     model.add(layers.Conv2DTranspose(
         32, (5, 5), strides=(2, 2), padding="same", use_bias=False, output_padding=0,
@@ -25,6 +53,7 @@ def make_generator_model():
     
     assert model.output_shape == (None, 125, 125, 32)
     
+    
     model.add(layers.Conv2DTranspose(
         16, (5, 5), strides=(2, 2), padding="same", use_bias=False, 
         kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.0002, seed=None)))
@@ -32,6 +61,7 @@ def make_generator_model():
     model.add(layers.ReLU())
     
     assert model.output_shape == (None, 250, 250, 16)
+    
     
     model.add(layers.Conv2DTranspose(
         8, (5, 5), strides=(2, 2), padding="same", use_bias=False, 
@@ -41,16 +71,29 @@ def make_generator_model():
     
     assert model.output_shape == (None, 500, 500, 8)
     
+    
     model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
     
     assert model.output_shape == (None, 1000, 1000, 1)
+    
 
     return model
 
 
 def make_discriminator_model():
     
+    """Creates a convolutional neural network which serves as discriminator model
+    
+    
+    Returns
+    -------
+    model : tf.keras.Sequential
+        The discriminator model
+        
+    """
+    
     model = tf.keras.Sequential()
+    
     
     model.add(layers.Conv2D(
         32, (5, 5), strides=(2, 2), padding='same', input_shape=[1000, 1000, 1], use_bias=False, 
@@ -58,6 +101,7 @@ def make_discriminator_model():
     model.add(layers.LeakyReLU(alpha=0.2))
     
     assert model.output_shape == (None, 500, 500, 32)
+    
 
     model.add(layers.Conv2D(
         64, (5, 5), strides=(2, 2), padding='same', use_bias=False, 
@@ -67,6 +111,7 @@ def make_discriminator_model():
     
     assert model.output_shape == (None, 250, 250, 64)
     
+    
     model.add(layers.Conv2D(
         128, (5, 5), strides=(2, 2), padding='same', use_bias=False, 
         kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.0002, seed=None)))
@@ -75,6 +120,7 @@ def make_discriminator_model():
     
     assert model.output_shape == (None, 125, 125, 128)
     
+    
     model.add(layers.Conv2D(
         256, (5, 5), strides=(2, 2), padding='same', use_bias=False, 
         kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.0002, seed=None)))
@@ -82,29 +128,10 @@ def make_discriminator_model():
     model.add(layers.LeakyReLU(alpha=0.2))
     
     assert model.output_shape == (None, 63, 63, 256)
+    
 
     model.add(layers.Flatten())
     model.add(layers.Dense(1))
 
     return model
-
-
-def discriminator_loss(real_output, fake_output):
-    
-    # This method returns a helper function to compute cross entropy loss
-    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-    
-    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
-    
-    total_loss = real_loss + fake_loss
-    return total_loss
-
-
-def generator_loss(fake_output):
-    
-    # This method returns a helper function to compute cross entropy loss
-    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-    
-    return cross_entropy(tf.ones_like(fake_output), fake_output)
 
---
+++ DCGAN/dcgan_train.py
@@ -0,0 +1,244 @@
+"""DCGAN Train
+
+This file contains functions to train the DCGAN. The code is based on the DCGAN tutorial on the TensorFlow website (see
+https://www.tensorflow.org/alpha/tutorials/generative/dcgan). 
+
+"""
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# !pip install -q tensorflow-gpu==2.0.0-alpha0
+import tensorflow as tf
+
+import os
+
+from dcgan_utils import load_train_test_dataset
+
+from dcgan_model import make_generator_model
+from dcgan_model import make_discriminator_model
+
+from argparse import ArgumentParser
+
+
+# File paths
+
+parser_dcgan = ArgumentParser()
+
+parser_dcgan.add_argument("--path_csv_labeled", default="/cluster/home/hannepfa/cosmology_aux_data/labeled.csv", type=str)
+parser_dcgan.add_argument("--dir_labeled_images", default="/cluster/home/hannepfa/cosmology_aux_data/labeled", type=str)
+parser_dcgan.add_argument("--path_csv_scored", default="/cluster/home/hannepfa/cosmology_aux_data/scored.csv", type=str)
+parser_dcgan.add_argument("--dir_scored_images", default="/cluster/home/hannepfa/cosmology_aux_data/scored", type=str)
+
+parser_dcgan.add_argument("--path_ckpt", 
+                        default="/cluster/home/hannepfa/CIL_project/DCGAN/ckpt/checkpoint", type=str)
+parser_dcgan.add_argument("--path_ckpt_stable", 
+                        default="/cluster/home/hannepfa/CIL_project/DCGAN/ckpt_stable/checkpoint", type=str)
+
+parser_dcgan.add_argument("--output_dir_generated_images", 
+                        default="/cluster/home/hannepfa/CIL_project/DCGAN/generated", type=str)
+parser_dcgan.add_argument("--path_pretrained_random_forest", 
+                        default="/cluster/home/hannepfa/CIL_project/RandomForest/random_forest_10_0.9.sav", type=str)
+parser_dcgan.add_argument("--path_scorefile", 
+                        default="/cluster/home/hannepfa/CIL_project/DCGAN/scorefile.csv", type=str)
+
+parser_dcgan.add_argument("--frac_train", default=0.9, type=float)
+
+
+# Hyperparameters
+
+_generator_optimizer = tf.keras.optimizers.Adam(lr=2e-4, beta_1=0.5)
+_discriminator_optimizer = tf.keras.optimizers.Adam(lr=2e-4, beta_1=0.5)
+
+_epochs = 80
+_batch_size = 24
+
+_latent_dim = 100
+
+
+# Metrics
+
+_gen_loss_mean = tf.keras.metrics.Mean('gen_loss_mean', dtype=tf.float32)
+_disc_loss_mean = tf.keras.metrics.Mean('disc_loss_mean', dtype=tf.float32)
+
+
+def _compute_generator_loss(fake_output):
+    
+    """Computes the loss of the generator model
+    
+                
+    Parameters
+    ----------
+    fake_output : tf.Tensor
+        The output of the discriminator for a batch of generated images
+                
+                
+    Returns
+    -------
+    gen_loss : tf.Tensor
+        The computed loss
+        
+    """
+    
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    
+    gen_loss = cross_entropy(tf.ones_like(fake_output), fake_output)
+    
+    return gen_loss
+
+
+def _compute_discriminator_loss(real_output, fake_output):
+    
+    """Computes the loss of the discriminator model
+    
+                
+    Parameters
+    ----------
+    real_output : tf.Tensor
+        The output of the discriminator for a batch of real images
+    fake_output : tf.Tensor
+        The output of the discriminator for a batch of generated images
+                
+                
+    Returns
+    -------
+    disc_loss : tf.Tensor
+        The computed loss
+        
+    """
+    
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    
+    disc_loss = real_loss + fake_loss
+    
+    return disc_loss
+
+
+@tf.function
+def train_step(batch_images, generator, discriminator):
+    
+    """Trains the generator model and the discriminator model of the DCGAN on a batch of images
+    
+                
+    Parameters
+    ----------
+    batch_images : tf.Tensor
+        The batch of images
+    generator : tf.keras.Sequential
+        The generator model
+    discriminator : tf.keras.Sequential
+        The discriminator model
+
+        
+    """
+    
+    noise = tf.random.normal([_batch_size, _latent_dim]) # dim(z) = _latent_dim
+    
+    
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        
+        generated_images = generator(noise, training=True)
+       
+        real_output = discriminator(batch_images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+        
+        gen_loss = _compute_generator_loss(fake_output)
+        disc_loss = _compute_discriminator_loss(real_output, fake_output)
+        
+        
+    gradients_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    
+    _generator_optimizer.apply_gradients(zip(gradients_generator, generator.trainable_variables))
+    _discriminator_optimizer.apply_gradients(zip(gradients_discriminator, discriminator.trainable_variables))
+    
+    
+def train_dcgan(arguments, generator, discriminator):
+    
+    """Trains the DCGAN on a all labeled images and on all scored images with score >= 2.61
+    
+                
+    Parameters
+    ----------
+    arguments : argparse-arguments
+        The given command-line arguments
+    generator : tf.keras.Sequential
+        The generator model
+    discriminator : tf.keras.Sequential
+        The discriminator model
+
+        
+    """
+    
+    train_dataset, test_dataset = load_train_test_dataset(arguments, _batch_size)
+    
+    _gen_loss_mean.reset_states()
+    _disc_loss_mean.reset_states()
+    
+    gen_loss_test = []
+    disc_loss_test = []
+    
+    checkpoint = tf.train.Checkpoint(generator=generator, discriminator=discriminator)
+    
+    
+    for epoch in range(1, _epochs + 1):
+        
+        for batch_images in train_dataset:
+            
+            train_step(batch_images, generator, discriminator)
+            
+
+        for batch_images in test_dataset:
+            
+            noise = tf.random.normal([_batch_size, _latent_dim]) # dim(z) = _latent_dim
+            
+            generated_images = generator(noise)
+       
+            real_output = discriminator(batch_images)
+            fake_output = discriminator(generated_images)
+        
+            gen_loss = _compute_generator_loss(fake_output)
+            disc_loss = _compute_discriminator_loss(real_output, fake_output)
+            
+            _gen_loss_mean.update_state(gen_loss)
+            _disc_loss_mean.update_state(disc_loss)
+         
+        gen_loss_test.append(_gen_loss_mean.result())
+        disc_loss_test.append(_disc_loss_mean.result())
+        
+        _gen_loss_mean.reset_states()
+        _disc_loss_mean.reset_states()
+            
+        
+        if epoch % 5 == 0:
+            
+            checkpoint.save(file_prefix=arguments.path_ckpt)
+            
+    
+    return gen_loss_test, disc_loss_test
+
+
+if __name__ == "__main__":
+    
+    arguments = parser_dcgan.parse_args()
+    
+    
+    generator = make_generator_model()
+    discriminator = make_discriminator_model()
+    
+
+    gen_loss_test, disc_loss_test = train_dcgan(arguments, generator, discriminator)
+    
+    
+    with open("/cluster/home/hannepfa/CIL_project/DCGAN/gen_loss_test.csv", "w") as f:
+        for item in gen_loss_test:
+            f.write("{}\n".format(item))
+     
+    with open("/cluster/home/hannepfa/CIL_project/DCGAN/disc_loss_test.csv", "w") as f:
+        for item in disc_loss_test:
+            f.write("{}\n".format(item))
+            
+            
